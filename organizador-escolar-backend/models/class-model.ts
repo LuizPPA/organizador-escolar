96406832182516d4aacca950aeb02834eeae2e33
Caio Campos <caio.campos@dcc.ufmg.br>
2021-06-15 20:49:10 -0300

    Time to Date type and added attribute link to class-model.ts

--- organizador-escolar-backend/models/class-model.ts
+++ organizador-escolar-backend/models/class-model.ts
@@ -3,9 +3,10 @@ import mongoose from 'mongoose';
 const classSchema = new mongoose.Schema({
     name: { type: String, require: true},
     code: { type: String, require: true, index: true},
-    syllabus: { type: String, require: true},
+    syllabus: { type: String, require: false},
     professor: { type: String, require: true},
-    time: { type: String, require: true}
+    time: [{ type: Date, require: true}],
+    link: [{type: String, require: true}]
 });
 
 export const User = mongoose.model('Class', classSchema);
