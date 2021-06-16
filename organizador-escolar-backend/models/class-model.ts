83a0849071b2343f9c6719f98ff78d9e03347c80
Caio Campos <caio.campos@dcc.ufmg.br>
2021-06-15 21:17:31 -0300

    Added link's limit in classSchema

--- organizador-escolar-backend/models/class-model.ts
+++ organizador-escolar-backend/models/class-model.ts
@@ -6,7 +6,17 @@ const classSchema = new mongoose.Schema({
     syllabus: { type: String, require: false},
     professor: { type: String, require: true},
     time: [{ type: Date, require: true}],
-    link: [{type: String, require: true}]
+    link: {
+        type: [{ 
+            type: String,
+            require: true
+        }], 
+        validate: [arrayLimit, '{PATH} exceeds the limit of 10']}
 });
 
 export const User = mongoose.model('Class', classSchema);
+
+// limit of 5 links per class
+function arrayLimit(val: string | any[]) {
+    return val.length <= 5;
+  }
